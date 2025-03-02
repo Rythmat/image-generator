1997a6b513170e170c84c5d5f72904b2df9418bd
Rythmat <teotaniwha@gmail.com>
2025-03-01 19:26:23 -0700

    Split functionality

---
+++ dynamicGen.py
@@ -0,0 +1,148 @@
+import sys
+
+#parameters to indicate png size
+width = int(1280)
+height = int(1280)
+
+#The number of squares on the side of an image
+div = 16
+numSquares = div*div
+
+#The pixel lengths of a square in the image 
+xLen = int(width/div)
+yLen = int(height/div)
+
+#list of tuples containing the square number, the x coordinate of the starting corner, and the y coordinate of the starting corner
+sqList = []
+#list of tuples containing the specified color, the x coordinate of the starting corner, and the y coordinate of the starting corner 
+sqCol = []
+#list of tuples containing the square number, and its specified color
+q1 = []
+
+#list of tuples containing the square number, the x coordinate of the starting corner, and the y coordinate of the starting corner if the top corner was indexed as (0,0)
+# sqMax = []
+
+
+#A function to dynamically change the color of the next square
+rVal = 50
+gVal = 50
+bVal = 50
+def generateColor():
+    global rVal, gVal, bVal
+    rVal *= 1.25
+    bVal += rVal
+    gVal *= 1.66
+    if(rVal>220):
+        rVal = 50
+    if(gVal>220):
+        gVal = 50
+    if(bVal>220):
+        gVal = 50
+    
+
+#Generates list of squares with corner and number determined by the number of divisions.
+def genSquares():
+        for i in range(1, numSquares+1):
+            #If the square is in the last column: special casing
+            if(i%div==0):
+                xco = (div-1)*xLen 
+                if (i/div) > (div/2):
+                    yco = -1*(yLen)
+                else:
+                    yco = 0
+                yco += (int)(i/div)*(yLen)
+                # ymax = (int)(i/div)*(yLen)
+                # xmax = width
+            #If the square is not in the last column
+            else:
+                xco = (i%div)*xLen
+                # xmax=xco
+                yco = ((int)(i/div))*yLen
+                if (i/div) < (div/2):
+                    yco += yLen
+                if (i%div) > (div/2):
+                    xco += -1*(xLen)
+                # ymax = ((int)(i/div)+1)*(yLen)
+            #Centering around a coordinate rather than propogating from a corner 
+            yco += -1*yLen*(div/2)
+            xco += -1*xLen*(div/2)
+            yco *= -1
+            # sqMax.append((i, xmax, ymax))
+            sqList.append((i, xco, yco))
+            # Filling color value for the first quadrant
+            if( (i%div<=(div/2)) and (int(i/div)<(div/2))):
+                q1.append((i,f"{rVal} {gVal} {bVal}\t"))
+                generateColor()
+        for i in sqList:
+            sqCol.append(i)
+
+
+
+
+
+#Returns the index of the specified square number in the provided list
+def findInd(num,list):
+    for ind,i in enumerate(list):
+        if int(i[0])==int(num):
+            return ind
+
+#Repeats the color pattern from the first quadrant in a mirrored fashion across the quadrants
+def mirrorColors():
+    for pos in q1:
+        for i,sq in enumerate(sqList):
+            xco = sqList[findInd(pos[0],sqList)][1]
+            yco = sqList[findInd(pos[0],sqList)][2]
+            if((abs(sq[1]))==(abs(xco)) and (abs(sq[2]))==(abs(yco))):
+                temp = list(sq)
+                temp[0] = pos[1]
+                sqCol[i] = tuple(temp)
+                continue
+
+        
+
+
+
+
+
+def printGrid():
+    file = open('grid.ppm','w')
+    sys.stdout = file
+    start = 0
+    print('P3')
+    print(width,height)
+    print('255')
+    while(start<(numSquares-1)):
+        colors = []
+        for i in range(start, start+div):
+            colors.append(sqCol[i][0])
+        for row in range(int(height/div)):
+            for c in colors:
+                for col in range(int(width/div)):
+                    print(c,end='')
+            print('')
+        start += div
+
+def printFun():
+    #Sets file output of ppm file
+    file = open('art.ppm','w')
+    sys.stdout = file
+
+
+    #Prints header for PPM file
+    print('P3')
+    print(width,height)
+    print('255')
+
+    #Initialize start index
+    for row in range(int(height)):
+        for col in range(int(width)):
+            print(f"{rVal} {gVal} {bVal}\t",end='')
+            if(col%10==0):
+                generateColor()
+    
+
+
+# genSquares()
+# mirrorColors()
+# printGrid()
+printFun()
--- gridGen.py
+++ gridGen.py
@@ -3,31 +3,37 @@ import sys
 #parameters to indicate png size
 width = int(1280)
 height = int(1280)
-
-#list of tuples containing the square number and its maximum x and y corner
-sqList = []
-sqMax = []
-sqCol = []
-q1 = []
-q1Col = []
+# Number of square per row and per column
 div = 16
-printList = []
+# The pixel lengths of a square in the final image
+xLen = int(width/div)
+yLen = int(height/div)
 
+#list of tuples containing the square number, the x coordinate of the starting corner, and the y coordinate of the starting corner
+sqList = []
+#list of tuples containing the specified color, the x coordinate of the starting corner, and the y coordinate of the starting corner 
+sqCol = []
+#list of tuples containing the square number, and its specified color
+q1 = []
+
+
+# A color key with the RBG values where 0 is white, 1 is black, 2 is green, and 3 is red
 colorKey = {
         0: '255 255 255\t',
         1: '0 0 0\t',
         2: '76 153 0\t',
         3: '153 76 0\t'
 }
-xLen = int(width/div)
-yLen = int(height/div)
+# The squares in the first quadrant specified to be a certain color
 blacks = [103,118,88,69,55,100,54,84,35,24,114,18,19,34,22,82]
 whites = [120,72,117,86,39,99,52,53,68,7,97,1,3,33,81,6]
 reds = [104,87,101,70,56,115,36,37,83,8,98,4,5,50,66,2]
 greens = [119,102,71,85,116,40,51,67,38,23,113,20,21,49,65,17]
+
+
 #Generates list of squares with corner and number determined by the number of divisions.
 def genSquares():
-        numSquares = 256
+        numSquares = div*div
         for i in range(1, numSquares+1):
             #If the square is in the last column: special casing
             if(i%div==0):
@@ -37,22 +43,18 @@ def genSquares():
                 else:
                     yco = 0
                 yco += (int)(i/div)*(yLen)
-                ymax = (int)(i/div)*(yLen)
-                xmax = width
             #If the square is not in the last column
             else:
-                xco=xmax = (i%div)*xLen
+                xco = (i%div)*xLen
                 yco = ((int)(i/div))*yLen
                 if (i/div) < (div/2):
                     yco += yLen
                 if (i%div) > (div/2):
                     xco += -1*(xLen)
-                ymax = ((int)(i/div)+1)*(yLen)
             #Centering around a coordinate rather than propogating from a corner 
             yco += -1*yLen*(div/2)
             xco += -1*xLen*(div/2)
             yco *= -1
-            sqMax.append((i, xmax, ymax))
             sqList.append((i, xco, yco))
             if( (i%div<=8) and (int(i/div)<8)):
                 if i in blacks:
@@ -70,10 +72,30 @@ def genSquares():
         for i in sqList:
             sqCol.append(i)
 
+#Returns the index of the specified square number in the provided list
+def findInd(squareNum,list):
+    for index,square in enumerate(list):
+        if int(square[0])==int(squareNum):
+            return index
+
+
+#Populates the color pattern from the first quadrant in a mirrored fashion across the quadrants
+def mirrorColors():
+    for pos in q1:
+        for i,sq in enumerate(sqList):
+            xco = sqList[findInd(pos[0],sqList)][1]
+            yco = sqList[findInd(pos[0],sqList)][2]
+            if((abs(sq[1]))==(abs(xco)) and (abs(sq[2]))==(abs(yco))):
+                temp = list(sq)
+                temp[0] = pos[1]
+                sqCol[i] = tuple(temp)
+                continue
+
+
+
 def printGrid():
-    orig_stdout = sys.stdout
-    f = open('grid.ppm','w')
-    sys.stdout = f
+    outFile = open('grid.ppm','w')
+    sys.stdout = outFile
     start = 0
     print('P3')
     print(width,height)
@@ -90,22 +112,7 @@ def printGrid():
         start += div
 
     
-def mirrorColors():
-    for pos in q1:
-        for i,sq in enumerate(sqList):
-            xco = sqList[findInd(pos[0],sqList)][1]
-            yco = sqList[findInd(pos[0],sqList)][2]
-            if((abs(sq[1]))==(abs(xco)) and (abs(sq[2]))==(abs(yco))):
-                temp = list(sq)
-                temp[0] = pos[1]
-                sqCol[i] = tuple(temp)
-                continue
 
-
-def findInd(num,list):
-    for ind,i in enumerate(list):
-        if int(i[0])==int(num):
-            return ind
 
 genSquares()
 mirrorColors()
