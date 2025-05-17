6d7c756b0034423c86ea2425473d54337330a12c
Rythmat <teotaniwha@gmail.com>
2025-05-16 21:20:39 -0700

    fix url

--- earthMandala/src/App.jsx
+++ earthMandala/src/App.jsx
@@ -3,11 +3,14 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [imageSrc, setImageSrc] = useState(null);
+  const [type, setType] = useState("sixteens");
+  const [paused, setPaused] = useState(false)
+  const intervalRef = useRef(null);
   
   useEffect(() => {
     const updateImage = () => {
       const timestamp = new Date().getTime();
-      const url = `https://earth-mandalate.onrender.com/generate?${timestamp}`;
+      const url = `https://earth-mandalate.onrender.com/generate?type=${type}&t=${timestamp}`;
       const img = new Image();
 
       img.onload = () => {
@@ -16,23 +19,41 @@ function App() {
 
       img.onerror = () => {
         console.error("Image failed to load:", url);
-        setTimeout(updateImage, 2000);
+        if (!paused) {
+          setTimeout(updateImage, 2000);
+        }
       };
 
       img.src = url;
     };
+    if(!paused){
+      updateImage();
+      intervalRef.current = setInterval(updateImage, 5000);
+    }
+    
+    return () => clearInterval(intervalRef.current);
+  }, [type, paused]);
 
-    updateImage();
-    const interval = setInterval(updateImage, 5000);
-    return () => clearInterval(interval);
-  }, []);
 
+  const togglePause = () => {
+    setPaused((prev) => !prev);
+  };
 
   return (
     <>
       <div className="container">
-      {imageSrc && <img src={imageSrc} alt="Earth Mandala"/>}
-    </div>
+        <select onChange={(e) => setType(e.target.value)} value={type}>
+          <option value="eigths">Three Quadrisections</option>
+          <option value="sixteens">Four Quadrisections</option>
+          <option value="thirtytwos">Five Quadrisections</option>
+        </select>
+
+        <button onClick={togglePause}>
+          {paused ? "Resume" : "Pause"}
+        </button>
+
+        {imageSrc && <img src={imageSrc} alt="Earth Mandala"/>}
+      </div>
     </>
   )
 }
