f1d6b0bfff2c8a87261fa029cf973b7b3d84f675
Rythmat <teotaniwha@gmail.com>
2025-05-16 21:32:16 -0700

    import

--- earthMandala/src/App.jsx
+++ earthMandala/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 function App() {
   const [imageSrc, setImageSrc] = useState(null);
